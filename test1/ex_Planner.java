9d796f62b3b0bde46136a4c6ece20b7d79ea6e43
daijin-cat <shirakami653@gmail.com>
2023-08-13 14:47:00 +0900

    ex_Planner.java 파일 최적화

--- test1/ex_Planner.java
+++ test1/ex_Planner.java
@@ -1,50 +1,51 @@
 package test1;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 
 public class ex_Planner {
-    private static TravelPlanner travelPlanner = new TravelPlanner();
-    private static Scanner scanner = new Scanner(System.in);
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private static final TravelPlanner travelPlanner = new TravelPlanner();
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("1. 여행 일정 추가");
-            System.out.println("2. 여행지 추가");
-            System.out.println("3. 여행 일정 조회");
-            System.out.println("4. 여행지 조회");
-            System.out.println("5. 종료");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("1. 여행 일정 추가");
+                System.out.println("2. 여행지 추가");
+                System.out.println("3. 여행 일정 조회");
+                System.out.println("4. 여행지 조회");
+                System.out.println("5. 종료");
 
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+                int choice = scanner.nextInt();
+                scanner.nextLine();
 
-            switch (choice) {
-                case 1:
-                    addTrip();
-                    break;
-                case 2:
-                    addDestination();
-                    break;
-                case 3:
-                    showTrips();
-                    break;
-                case 4:
-                    showDestinations();
-                    break;
-                case 5:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("잘못된 입력입니다.");
-                    break;
+                switch (choice) {
+                    case 1:
+                        addTrip(scanner);
+                        break;
+                    case 2:
+                        addDestination(scanner);
+                        break;
+                    case 3:
+                        showTrips();
+                        break;
+                    case 4:
+                        showDestinations();
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        System.out.println("잘못된 입력입니다.");
+                        break;
+                }
             }
         }
     }
 
-    private static void addTrip() {
+    private static void addTrip(Scanner scanner) {
         System.out.print("여행 일정 이름: ");
         String name = scanner.nextLine();
 
@@ -60,7 +61,7 @@ public class ex_Planner {
         System.out.println("여행 일정이 추가되었습니다.");
     }
 
-    private static void addDestination() {
+    private static void addDestination(Scanner scanner) {
         System.out.print("여행지 이름: ");
         String name = scanner.nextLine();
 
@@ -76,7 +77,7 @@ public class ex_Planner {
     private static void showTrips() {
         ArrayList<Trip> trips = travelPlanner.getTrips();
 
-        if (trips.size() == 0) {
+        if (trips.isEmpty()) {
             System.out.println("등록된 여행 일정이 없습니다.");
             return;
         }
@@ -87,7 +88,7 @@ public class ex_Planner {
 
             ArrayList<Destination> destinations = trip.getDestinations();
 
-            if (destinations.size() == 0) {
+            if (destinations.isEmpty()) {
                 System.out.println("등록된 여행지가 없습니다.");
             } else {
                 System.out.println("등록된 여행지:");
@@ -104,7 +105,7 @@ public class ex_Planner {
     private static void showDestinations() {
         ArrayList<Destination> destinations = travelPlanner.getDestinations();
 
-        if (destinations.size() == 0) {
+        if (destinations.isEmpty()) {
             System.out.println("등록된 여행지가 없습니다.");
             return;
         }
@@ -119,7 +120,7 @@ public class ex_Planner {
     private static Date parseDate(String dateString) {
         try {
             return dateFormat.parse(dateString);
-        } catch (Exception e) {
+        } catch (ParseException e) {
             System.out.println("잘못된 날짜 형식입니다. (yyyy-MM-dd)");
             return null;
         }
